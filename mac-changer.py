41e56de8992c349dc8ecc037f1a15f6c57f2f6a2
Amaresh Rout <60665946+amaresh-git-dev@users.noreply.github.com>
2021-07-31 22:22:25 +0530

    Add files via upload

---
+++ mac-changer.py
@@ -0,0 +1,34 @@
+import subprocess
+
+def logo():
+	
+
+	print("""
+
+		                          _                                 
+		                         | |                                
+	  _ __ ___   __ _  ___ ______ ___| |__   __ _ _ __   __ _  ___ _ __ 
+	 | '_ ` _ \ / _` |/ __|______/ __| '_ \ / _` | '_ \ / _` |/ _ \ '__|
+	 | | | | | | (_| | (__      | (__| | | | (_| | | | | (_| |  __/ |   
+	 |_| |_| |_|\__,_|\___|      \___|_| |_|\__,_|_| |_|\__, |\___|_|   
+		                                             __/ |          
+		                  @amaresh-git-dev          |___/  
+		                                                     
+
+	""") 
+
+logo()
+
+print1 = print("* press 1 for change current mac")
+print2 = print("* press 2 to reset your parmanent mac")
+
+choice = int(input("Enter your choice: "))
+
+if choice == 1:
+	subprocess.call("sudo macchanger -a eth0",shell=True)
+	
+elif choice == 2:
+	subprocess.call("sudo macchanger -p eth0",shell=True)
+
+else:
+	print("Wrong option")
